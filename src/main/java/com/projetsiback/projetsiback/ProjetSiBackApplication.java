9285da666fc0a2809b8a206c63055346f731f69b
Yracerlyn <yessicerlyn@gmail.com>
2024-05-05 18:42:48 +0200

    fist commit

---
+++ src/main/java/com/projetsiback/projetsiback/ProjetSiBackApplication.java
@@ -0,0 +1,13 @@
+package com.projetsiback.projetsiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSiBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetSiBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetsiback/projetsiback/ProjetSiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetsiback.projetsiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSiBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
